bcb4199fbd4ac2ef57c0f458981bca12efef70e1
spencermuparutsa-create <spencer.muparutsa@gmail.com>
2025-11-02 10:00:48 +0100

    Add files via upload

--- homework.py
+++ homework.py
@@ -13,12 +13,46 @@ cat.y = random.randint(0,500)
 mouse.x = random.randint(0,500)
 mouse.y = random.randint(0,500)
 
+score = 0
+gameover = False
 
 def draw():
 
     screen.blit("background",(0,0))
     cat.draw()
     mouse.draw()
+    screen.draw.text("Score:" + str(score),(50,50))
+    if gameover == True:
+        screen.fill("purple")
+        screen.draw.text("Time's up! Your score is " + str(score),(50,50))
+
+
+
+
+def update(): 
+    global score
+    if keyboard.a:
+        cat.x -= 5
+
+    if keyboard.d:
+        cat.x += 5
+
+    if keyboard.w:
+        cat.y -= 5
+    
+    if keyboard.s:
+        cat.y += 5
+
+    if cat.colliderect(mouse):
+        mouse.x = random.randint(0,500)
+        mouse.y = random.randint(0,500)
+        score += 10
+
+def timer():
+    global gameover
+    gameover = True
+
+clock.schedule(timer,20.0)
 
 
 pgzrun.go()
